173c921ee71365b6c43fed6df7037d067c944abb
vlewin <vlewin@suse.de>
2011-08-31 23:10:00 +0200

    - proof of concept (samba shares - index)

--- app/controllers/browser_controller.rb
+++ app/controllers/browser_controller.rb
@@ -1,12 +1,8 @@
 class BrowserController < ApplicationController
-#  USER = ENV['USER']
-#  HOME = ENV['HOME'] #or File.expand_path('~')
-  
   def get
     browser = Browser.new('/')
     @dirs = browser.get_content(params["dir"]);
     render :json => @dirs.to_json
   end
-  
 end
 
